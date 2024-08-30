9276cf655649e66a3b7754cee55826f18c32f483
Salman Falah Taqiyuddin <salman_taqiyuddin_33rpl@student.smktelkom-mlg.sch.id>
2024-08-30 09:35:01 +0700

    Add files via upload

---
+++ Operator.java
@@ -0,0 +1,29 @@
+public class Operator {
+    public static void main(String[] args) {
+        // System.out.println("Operator Aritmatika dan Penugasan");
+        // int a, b, c;
+
+        // a=5;
+        // c=5%2;
+        // b=6*7;
+        // c+=5;
+        // a-=1;
+
+        // System.out.println(a);
+        // System.out.println(b);
+        // System.out.println(c);
+        
+        System.out.println("Operator perbandingan");
+        int d, e, f;
+
+        d=7;
+        e=3;
+        f=1;
+
+        System.out.println(d == e);
+        System.out.println(d != d);
+        System.out.println(d > f);
+        System.out.println(d <= f);
+
+    }
+}
---
+++ percabanganIf.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class percabanganIf {
+    public static void main(String[] args) {
+    double total;
+
+    Scanner s = new Scanner(System.in);
+    System.out.println("Masukkan total: ");
+    total = s.nextDouble();
+
+    if(total >100000){
+        System.out.println("Anda mendapatkan diskon 10%");
+
+     }
+    }
+}
